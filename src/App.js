66345998244e72cc34edcdab4b08fadcdce00027
Christine Le Guennec <christineleguennec@mbpdechristine.lan>
2019-05-27 12:34:58 +0200

    première version ajout talent

--- src/App.js
+++ src/App.js
@@ -1,26 +1,243 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import ReactFileReader from "react-file-reader";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    status: "write" /* ou read */,
+    picture: null,
+    firstName: "",
+    lastName: "",
+    linkedIn: "",
+    eamil: "",
+    phone: "",
+    wage: "",
+    curCompany: "",
+    sector: "",
+    curPosition: "",
+    desPosition: "",
+    availability: 0,
+    idealFirm: "",
+    idealRole: "",
+    idealEnvironment: "",
+    ambitions: "",
+    hardSkills: [],
+    softSkills: []
+  };
+
+  onClick = () => {
+    axios.post("https://ernest-server.herokuapp.com/talent/create", {
+      informations: {
+        firstName: String,
+        lastName: String,
+        phoneNumber: String,
+        actualCompany: String,
+        mobility: String,
+        actualTitle: String,
+        wantedTitle: String
+      },
+      description: {
+        idealCompany: String,
+        idealRole: String,
+        workingEnvironment: String,
+        development: String
+      },
+      skills: {
+        soft: Array,
+        hard: Array
+      }
+    });
+  };
+
+  handleFiles = files => {
+    this.setState({
+      picture: files.base64
+    });
+  };
+
+  render() {
+    return (
+      <div className="content">
+        {/* left section */}
+        <div className="leftContainer">
+          {/*    <img
+            src="https://images.pexels.com/photos/1520760/pexels-photo-1520760.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=50&w=50"
+            alt="portrait of talent"
+          /> */}
+          <div>
+            <ReactFileReader
+              fileTypes={[".png", ".jpg"]}
+              base64={true}
+              multipleFiles={false}
+              handleFiles={this.handleFiles}
+            >
+              {this.state.picture !== null ? (
+                <span>
+                  <img src={this.state.picture} alt="portrait of talent" />
+                </span>
+              ) : (
+                <div
+                  style={{
+                    height: "50px",
+                    width: "50px",
+                    backgroundColor: "red"
+                  }}
+                />
+              )}
+            </ReactFileReader>
+          </div>
+
+          <form className="talentDetails">
+            <input
+              name="First Name"
+              value={this.state.firstName}
+              placeholder="Prénom"
+              onChange={e => {
+                this.setState({ firstName: e.target.value });
+              }}
+            />
+            <input
+              name="Last Name"
+              value={this.state.lastName}
+              placeholder="NOM"
+              onChange={e => {
+                this.setState({ lastName: e.target.value });
+              }}
+            />
+            <input
+              name="LinkedIn Profil"
+              value={this.state.linkedIn}
+              placeholder="Profil LinkedIn"
+              onChange={e => {
+                this.setState({ linkedIn: e.target.value });
+              }}
+            />
+            <input
+              name="email"
+              value={this.state.email}
+              placeholder="adresse@email.com"
+              onChange={e => {
+                this.setState({ email: e.target.value });
+              }}
+            />
+            <input
+              name="phone number"
+              value={this.state.phone}
+              placeholder="0XXXXXXX"
+              onChange={e => {
+                this.setState({ phone: e.target.value });
+              }}
+            />
+            <input
+              name="Wage"
+              value={this.state.wage}
+              placeholder="$$$$$$$$"
+              onChange={e => {
+                this.setState({ wage: e.target.value });
+              }}
+            />
+            <input
+              name="Current company"
+              value={this.state.curCompany}
+              placeholder="Entreprise actuelle"
+              onChange={e => {
+                this.setState({ curCompany: e.target.value });
+              }}
+            />
+            <input
+              name="Desired sector"
+              value={this.state.sector}
+              placeholder="Secteur Souhaité"
+              onChange={e => {
+                this.setState({ sector: e.target.value });
+              }}
+            />
+            <input
+              name="Current position"
+              value={this.state.curPosition}
+              placeholder="Fonction actuelle"
+              onChange={e => {
+                this.setState({ curPosition: e.target.value });
+              }}
+            />
+            <input
+              name="Desired Position"
+              value={this.state.desPosition}
+              placeholder="Position souhaitée"
+              onChange={e => {
+                this.setState({ desPosition: e.target.value });
+              }}
+            />
+          </form>
+          <div className="availability">
+            <div
+              onClick={() => {
+                this.setState({ availability: 1 });
+              }}
+              style={{
+                backgroundColor: this.state.availability === 1 ? "red" : "white"
+              }}
+            />
+            <div
+              onClick={() => {
+                this.setState({ availability: 2 });
+              }}
+              style={{
+                backgroundColor:
+                  this.state.availability === 2 ? "blue" : "white"
+              }}
+            />
+            <div
+              onClick={() => {
+                this.setState({ availability: 3 });
+              }}
+              style={{
+                backgroundColor:
+                  this.state.availability === 3 ? "black" : "white"
+              }}
+            />
+            <div
+              onClick={() => {
+                this.setState({ availability: 4 });
+              }}
+              style={{
+                backgroundColor:
+                  this.state.availability === 4 ? "green" : "white"
+              }}
+            />
+          </div>
+        </div>
+
+        {/* right section */}
+        <div>
+          <form>
+            <textarea name="ideal firm" value={this.state.idealFirm} />
+            <textarea name="ideal role" value={this.state.idealRole} />
+            <div className="allWishes">
+              <div className="wishes">
+                <textarea
+                  name="ideal environment"
+                  value={this.state.idealEnvironment}
+                />
+                <textarea name="ambitions" value={this.state.ambitions} />
+              </div>
+              <div className="skills">
+                <textarea name="hardskills" value={this.state.hardSkills} />
+                <textarea name="softskills" value={this.state.softSkills} />
+              </div>
+            </div>
+          </form>
+          <div className="buttons">
+            <div className="cancel">X</div>
+            <div className="validate" onClick={this.onClick}>
+              Yes
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
