4daf617a7217cde7ab98fcfbfcac7e78a3c26b43
NamReact <49146106+NamReact@users.noreply.github.com>
2019-06-12 23:27:41 +0200

    Nam popup add client (#90)
    * redone popup css + reorganize code
    
    * debug bearer

--- src/components/ClientList/AddClientForm.js
+++ src/components/ClientList/AddClientForm.js
@@ -6,7 +6,7 @@ import "./AddClientForm.css";
 class AddClientForm extends React.Component {
   state = {
     entreprise: "",
-    secteur: "",
+    secteur: null,
     secteurId: "",
     taille: null,
     email: "",
@@ -39,23 +39,24 @@ class AddClientForm extends React.Component {
   };
 
   // fonction pour gérer secteur
-  // setSector = event => {
-  //   const value = event.target.value;
-  //   // console.log(id);
-  //   // const sectorArray = this.state.secteur;
-  //   // sectorArray.push(this.state.sectorList[id].name);
-  //   this.setState({
-  //     secteur: value,
-  //     sectorSelect: false
-  //   });
-  //   console.log("le secteur :");
-  //   console.log(this.state.secteur);
-  // };
+  setSector = event => {
+    let secteur;
+    for (let i = 0; i < this.state.sectorList.length; i++) {
+      if (event.target.id === this.state.sectorList[i]._id) {
+        secteur = this.state.sectorList[i];
+        break;
+      }
+    }
+    this.setState({
+      secteur,
+      sectorSelect: false
+    });
+  };
+
   /* Function to handle company size */
 
   handleSize = e => {
     const id = e.target.id;
-    console.log(id);
     this.setState({ taille: id, sizeSelect: false });
   };
 
@@ -89,7 +90,7 @@ class AddClientForm extends React.Component {
 
       {
         name: this.state.entreprise,
-        field: this.state.secteurId,
+        field: this.state.secteur._id,
         size: this.state.taille,
         email: this.state.email
       },
@@ -114,202 +115,95 @@ class AddClientForm extends React.Component {
       return false;
     }
 
-    // const sectorArray = this.state.sectorList.map((item, index) => {
-    //   return (
-    //     <div id={index} key={item._id} onClick={this.setSector}>
-    //       {item.name}
-    //     </div>
-    //   );
-    // });
+    const sectorArray = this.state.sectorList.map((item, index) => {
+      return (
+        <div
+          id={item._id}
+          key={item._id}
+          onClick={this.setSector}
+          className="add-client-sector-item"
+        >
+          {item.name}
+        </div>
+      );
+    });
 
     return (
-      <div className="popup-new-client">
-        <div className="addClientFormContainer">
-          {/* Header */}
-
-          <div className="formHeader">
-            <h4>Ajouter un nouveau client</h4>
-            <div className="closingButton" onClick={this.props.closePopup}>
-              <i className="fas fa-times" />
-            </div>
+      <div className="add-client-popup-container">
+        <div className="add-client-header">
+          <div className="add-client-title">Ajouter un nouveau client</div>
+          <div className="add-client-close" onClick={this.props.closePopup}>
+            <i className="fas fa-times" />
           </div>
-
-          <div className="formContainer">
-            <div>
-              <label className="addclient-form-label"> Entreprise</label>
-              <input
-                className="addclient-form-input"
-                value={this.state.entreprise}
-                onChange={this.handleChange}
-                placeholder="Entreprise"
-                type="text"
-                name="entreprise"
-                id="name"
-                required
-              />
+        </div>
+        <div className="add-client-input-container">
+          <div>Entreprise</div>
+          <input
+            value={this.state.entreprise}
+            onChange={this.handleChange}
+            placeholder="Entreprise"
+            type="text"
+            name="entreprise"
+            id="name"
+            required
+          />
+          <div>
+            <div>Secteur</div>
+            <div className="add-client-sector" onClick={this.sectorSelect}>
+              {this.state.secteur
+                ? this.state.secteur.name
+                : "Sélectionnez un secteur"}
             </div>
-            {/* className="addclient-form-whole-sector" */}
-            <div>
-              <div clasName="addclient-sector-big">
-                {/* liste des secteurs */}
-                <div className="sector-container">
-                  <div className="addclient-form-label"> Secteur</div>
-                  {/* <div className="sector-select" onClick={this.sectorSelect} /> */}
-                  <div className="sector-select" onClick={this.sectorSelect}>
-                    {this.state.secteur ? (
-                      <p className="placeholder-selector">
-                        {this.state.secteur}
-                      </p>
-                    ) : (
-                      <p className="placeholder-selector">
-                        Sélection du secteur
-                      </p>
-                    )}
-                  </div>
-                  <div>
-                    {this.state.sectorSelect && (
-                      <div className="overflow-sector" id={this.state.secteur}>
-                        {this.state.sectorList.map(item => {
-                          return (
-                            <div
-                              id={item.name}
-                              key={item._id}
-                              className="sector-item-array"
-                              onClick={e => {
-                                this.sectorSelect();
-                                this.setState({
-                                  secteur: item.name,
-                                  secteurId: item._id
-                                });
-                              }}
-                            >
-                              {item.name}
-                            </div>
-                          );
-                        })}
-                      </div>
-                    )
-
-                    //  : (
-                    //   false
-                    // )
-                    }
-                  </div>
-                </div>
-
-                {/* <select
-                value={this.state.secteur}
-                onChange={this.setSector}
-                type="text"
-                name="secteur"
-                id="secteur"
-                required
-              > */}
-                {/* {this.state.sectorList.map((sector, id) => {
-                  return (
-                    <option id={sector._id} key={sector._id} value={sector._id}>
-                      {sector.name}
-                    </option>
-                  );
-                })}
-              </select> */}
-
-                {/* Proposition : fenêtre d'ajout d'un nouveau secteur en mode apparition */}
-                <div className="addnewsector" onClick={this.ShowNewSector}>
-                  <div className="button-new-sector">
-                    <i className="fas fa-plus" />
-                    <p> Ajouter un nouveau secteur</p>
-                  </div>
-
-                  <div className="addsector-p" />
-                </div>
-              </div>
-
-              {this.state.addNewSectorDiv === true ? (
-                <div className="addclient-new-sector-container">
-                  <label className="addclient-form-label">
-                    Ajout d'un nouveau secteur
-                  </label>
-                  <input
-                    className="addclient-form-input"
-                    name="addNewSector"
-                    value={this.state.addNewSector}
-                    onChange={this.handleChange}
-                    type="texte"
-                    id="texte"
-                  />
-                </div>
-              ) : (
-                this.state.addNewSectorDiv
-              )}
-            </div>
-
-            {/* Taille de l'entreprise */}
-            <div className="size-container">
-              <div className="addclient-form-label">Taille</div>
-              <div className="size-button" onClick={this.sizeSelect}>
-                {this.state.taille ? (
-                  this.state.taille
-                ) : (
-                  <p>Sélection de la taille</p>
-                )}
-              </div>
-              <div>
-                {this.state.sizeSelect && (
-                  <div className="size-select">
-                    <div className="overflow-sector">
-                      <div
-                        className="sector-item-array"
-                        onClick={this.handleSize}
-                        id="Petite"
-                      >
-                        Petite
-                      </div>
-                      <div
-                        className="sector-item-array"
-                        onClick={this.handleSize}
-                        id="Grande"
-                      >
-                        Grande
-                      </div>
-                    </div>
-                  </div>
-                )}
-              </div>
-            </div>
-
-            <div className="addclient-container-email">
-              {/* Taille entreprise */}
-              <label className="addclient-form-label">Email du référent </label>
-              <input
-                value={this.state.email}
-                onChange={this.handleChange}
-                placeholder="adresse de messagerie"
-                type="email"
-                name="email"
-                id="email"
-                className="addclient-form-input"
-                required
-              />
-            </div>
-            <div className="submitButtons">
-              <div onClick={this.props.closePopup} className="cancel">
-                Annuler
+            {this.state.sectorSelect && (
+              <div className="add-client-sector-list">{sectorArray}</div>
+            )}
+          </div>
+          <div>Taille de l'entreprise</div>
+          <div className="add-client-sector" onClick={this.sizeSelect}>
+            {this.state.taille ? this.state.taille : "Sélectionnez une taille"}
+          </div>
+          {this.state.sizeSelect && (
+            <div className="add-client-size-list">
+              <div
+                className="add-client-size-item"
+                id="Petite"
+                onClick={this.handleSize}
+              >
+                Petite
               </div>
               <div
-                onClick={this.handleSubmit}
-                className="validate addClientPopUp"
+                className="add-client-size-item"
+                id="Grande"
+                onClick={this.handleSize}
               >
-                Ajouter
+                Grande
               </div>
             </div>
-
-            {/* affichage de l'état de la validation du formulaire */}
-            <div>
-              {this.state.error === true ? <p>error</p> : this.state.error}
-              {this.state.valid === true ? <p>validate</p> : this.state.valid}
-            </div>
+          )}
+          <div>Email</div>
+          <input
+            value={this.state.email}
+            onChange={this.handleChange}
+            placeholder="Email"
+            type="text"
+            name="email"
+            id="name"
+            required
+          />
+        </div>
+        <div className="add-client-buttons">
+          <div onClick={this.props.closePopup} className="add-client-cancel">
+            Annuler
           </div>
+          <div onClick={this.handleSubmit} className="add-client-validate">
+            Ajouter
+          </div>
+        </div>
+
+        {/* affichage de l'état de la validation du formulaire */}
+        <div>
+          {this.state.error === true ? <p>error</p> : this.state.error}
+          {this.state.valid === true ? <p>validate</p> : this.state.valid}
         </div>
       </div>
     );
